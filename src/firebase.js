986db4f69e2871ef2656426daded39951426effd
Connor Flynn <connorflynn4@gmail.com>
2021-04-26 22:53:39 -0700

    first

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_MKhtczLmtFwVudBmEb5jphk-0Xhuh00",
+    authDomain: "slack-react-clone-6c90a.firebaseapp.com",
+    projectId: "slack-react-clone-6c90a",
+    storageBucket: "slack-react-clone-6c90a.appspot.com",
+    messagingSenderId: "276702103891",
+    appId: "1:276702103891:web:78deee38f9cd4c13c679a6"
+  };
